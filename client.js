2dea268665dcf95ee4ba64ff0a990faad084323b
ryantussing <56258028+ryantussing@users.noreply.github.com>
2020-12-14 13:54:33 -0500

    .js is functioning, adding shopping bar now

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-cart = document.getElementById("pageBody")
+cart = document.getElementById("pageBody-cart")
 downArrow = document.getElementById("down-arrow-img")
 $shoppingCartIcon = document.querySelector("img#shoppingCartImg")
 
@@ -17,16 +17,16 @@ function addProducts() {
     const $pageBody = document.querySelector("div#pageBody")
 
     const $productContainer = document.createElement("div")
-    $productContainer.style.cssText = "height: auto; display: flex; flex-direction: row; flex-wrap: wrap; justify-content: space-evenly; padding-top: 1vh; padding-bottom: 1vh";
+    $productContainer.style.cssText = "height: auto; display: flex; flex-direction: row; flex-wrap: wrap; justify-content: space-evenly; padding-top: 1vh; padding-bottom: 1vh; text-align: center";
     $pageBody.append($productContainer)
 
     data.posters.forEach(poster => {
         const $section = document.createElement("section")
-        $section.style.cssText = "border: solid 1px rgb(230, 230, 255); width: 25vh; display: flex; flex-direction: column; flex-wrap: wrap; justify-content: space-evenly; padding-top: 1vh";
+        $section.style.cssText = "border: solid 1px rgb(230, 230, 255); width: 25vh; display: flex; flex-direction: column; flex-wrap: wrap; justify-content: center; padding-top: 1vh; margin-top: 5px; margin-bottom: 5px; item-align: center;"
 
         const $img = document.createElement("img")
-        $img.innerHTML = `<img src="${poster.imgSource}" alt="${poster.name}">`
-        $img.style.border = "1px solid black"
+        $img.src = poster.imgSource; $img.alt = poster.name
+        $img.style.cssText = "width:200px; height: 300px; text-align: center;"
         $section.append($img)
 
         const $h2 = document.createElement("h2")
@@ -34,6 +34,16 @@ function addProducts() {
         $h2.style.fontSize = "12px"
         $h2.style.textAlign = "center"
         $section.append($h2)
+
+        const $shoppingBar = document.createElement("section")
+        $shoppingBar.style.cssText = "border: solid 1px rgb(230, 230, 255); width: 25vh; display: flex; flex-direction: column; flex-wrap: wrap; justify-content: space-evenly; width: 20vw; text-align: center;"
+            const $plusSign = document.createElement("img")
+            $plusSign.src = "Plus.png"
+            const $minusSign = document.createElement("img")
+            $minusSign.src = "Minus.png"
+            $shoppingBar.append($plusSign)
+            $shoppingBar.append($minusSign)
+        $section.append($shoppingBar)
 
         $productContainer.append($section)
     })
@@ -70,7 +80,7 @@ function loadData() {
 
         {
             "name" : "Spiderverse Poster",
-            "imgSource" : "Pictures/AvengersPoster.jpg"
+            "imgSource" : "Pictures/SpiderversePoster.jpg"
         }
     ]
     return {
