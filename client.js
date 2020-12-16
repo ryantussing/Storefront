a34c941ada0fbd05d044ff1cbb14377d11c0b067
ryantussing <56258028+ryantussing@users.noreply.github.com>
2020-12-15 22:31:33 -0500

    added additional products, made shopping bar

--- client.js
+++ client.js
@@ -1,14 +1,28 @@
-cart = document.getElementById("pageBody-cart")
-downArrow = document.getElementById("down-arrow-img")
-$shoppingCartIcon = document.querySelector("img#shoppingCartImg")
+cart = document.getElementById("pageBody-cartDiv")
+$downArrow = document.getElementById("down-arrow-img")
+$shoppingCartIcon = document.getElementById("shoppingCartImg")
+var clicks = 2
 
-downArrow.onclick = onClickDownArrow
-$shoppingCartIcon = onClickDownArrow
-function onClickDownArrow () {
-    const $cartSection = document.createElement("section")
-    $cartSection.style.border = "1px solid black"
-    $cartSection.style.backgroundColor = "rgb(240, 240, 240)"
-    cart.append($cartSection)
+$downArrow.onclick = onArrow
+$shoppingCartIcon.onclick = onArrow
+function onArrow() {
+
+    // if ( (clicks%2) == 0) {
+        const $userCart = document.createElement("section")
+        $userCart.innerHTML = `<section id="userCart"></section>`
+        $userCart.style.border = "1px solid rgb(230, 230, 255)"
+        $userCart.style.backgroundColor = "rgb(240, 240, 240)"
+        $downArrow.src = "Pictures/UpArrow.png"
+        cart.append($userCart)
+        clicks++
+    // }
+    // else if ( (clicks%2) == 1) {
+    //     $userCart = document.querySelector("section#userCart")
+    //     $downArrow.src = "Pictures/DownArrow.png"
+    //     $userCart.parentNode.removeChild($userCart)
+    //     clicks++
+    // }
+
 }
 
 const data = loadData()
@@ -22,11 +36,11 @@ function addProducts() {
 
     data.posters.forEach(poster => {
         const $section = document.createElement("section")
-        $section.style.cssText = "border: solid 1px rgb(230, 230, 255); width: 25vh; display: flex; flex-direction: column; flex-wrap: wrap; justify-content: center; padding-top: 1vh; margin-top: 5px; margin-bottom: 5px; item-align: center;"
+        $section.style.cssText = "text-align: center; border: solid 1px rgb(230, 230, 255); width: 25vw; display: flex; flex-direction: column; flex-wrap: wrap; justify-content: center; padding-top: 1vh; padding-bottom: 1vh; margin-top: 5px; margin-bottom: 5px;"
 
         const $img = document.createElement("img")
         $img.src = poster.imgSource; $img.alt = poster.name
-        $img.style.cssText = "width:200px; height: 300px; text-align: center;"
+        $img.style.cssText = "width:12vh; height: 18vh; text-align: center; border: 1px solid black;"
         $section.append($img)
 
         const $h2 = document.createElement("h2")
@@ -36,13 +50,19 @@ function addProducts() {
         $section.append($h2)
 
         const $shoppingBar = document.createElement("section")
-        $shoppingBar.style.cssText = "border: solid 1px rgb(230, 230, 255); width: 25vh; display: flex; flex-direction: column; flex-wrap: wrap; justify-content: space-evenly; width: 20vw; text-align: center;"
-            const $plusSign = document.createElement("img")
-            $plusSign.src = "Plus.png"
-            const $minusSign = document.createElement("img")
-            $minusSign.src = "Minus.png"
-            $shoppingBar.append($plusSign)
-            $shoppingBar.append($minusSign)
+        $shoppingBar.style.cssText = "border: solid 1px rgb(230, 230, 255); width: 20vw; display: flex; flex-direction: row; flex-wrap: wrap; justify-content: space-between; width: 20vw; text-align: center; padding: 1px;"
+            const $plus = document.createElement("img")
+                $plus.src = "Pictures/Minus.png"
+                $plus.style.cssText = "width: 20px; height: 20px;"
+            $shoppingBar.append($plus)
+            const $shoppingBarText = document.createElement("h3")
+                $shoppingBarText.innerHTML = "<h3>Q U A N T I T Y</h3>"
+                $shoppingBarText.style.cssText = "font-size: 12px;"
+            $shoppingBar.append($shoppingBarText)
+            const $minus = document.createElement("img")
+                $minus.src = "Pictures/Plus.png"
+                $minus.style.cssText = "width: 20px; height: 20px; line-height: 0;"
+            $shoppingBar.append($minus)
         $section.append($shoppingBar)
 
         $productContainer.append($section)
@@ -79,8 +99,53 @@ function loadData() {
         },
 
         {
+            "name" : "Pokemon Poster",
+            "imgSource" : "Pictures/PokemonPoster.jpg"
+        },
+
+        {
             "name" : "Spiderverse Poster",
             "imgSource" : "Pictures/SpiderversePoster.jpg"
+        },
+
+        {
+            "name" : "Die Hard Poster",
+            "imgSource" : "Pictures/DieHardPoster.jpg"
+        },
+
+        {
+            "name" : "Toy Story Poster",
+            "imgSource" : "Pictures/ToyStoryPoster.jpg"
+        },
+
+        {
+            "name" : "Guardians of the Galaxy Poster",
+            "imgSource" : "Pictures/GOTGPoster.jpg"
+        },
+
+        {
+            "name" : "Khalid Poster",
+            "imgSource" : "Pictures/KhalidPoster.jpg"
+        },
+
+        {
+            "name" : "Fortnite Poster",
+            "imgSource" : "Pictures/FortnitePoster.jpg"
+        },
+
+        {
+            "name" : "Cyberpunk 2077 Poster",
+            "imgSource" : "Pictures/CyberpunkPoster.jpg"
+        },
+
+        {
+            "name" : "Home Alone Poster",
+            "imgSource" : "Pictures/HomeAlonePoster.jpg"
+        },
+
+        {
+            "name" : "The Last Of Us Poster",
+            "imgSource" : "Pictures/TLOUPoster.jpg"
         }
     ]
     return {
